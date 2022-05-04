01bfc89d0408875a4181dea84ece5b9b0026773e
quangcm <chauminhquang99@gmail.com>
2022-05-04 14:38:38 +0700

    Create folders and new header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import IndexPage from "./pages/home-page-v3";
+import AboutPage from "./pages/about-us-v3";
 
 function App() {
   return (
     <>
-      <IndexPage />
+      {/* <IndexPage /> */}
+      <AboutPage />
     </>
   );
 }
---
+++ src/pages/about-us-v3/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.scss";
+
+import SectionHeader from "./body/section-header";
+import SectionIntro from "./body/section-intro";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <SectionHeader />
+      <SectionIntro />
+    </main>
+  );
+};
+
+export default AboutPage;
