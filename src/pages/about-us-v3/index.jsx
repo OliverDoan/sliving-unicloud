fa33c2734ebe269e3c88da3f150324cd6b2d66df
OliverDoan <doanvietthuan1005@gmail.com>
2022-05-05 16:37:37 +0700

    add section brand story

--- src/pages/about-us-v3/index.jsx
+++ src/pages/about-us-v3/index.jsx
@@ -8,11 +8,13 @@ import SectionHelpMore from "../../components/help-more/section-help-more";
 import SectionOurPartner from "./body/section-our-partner";
 import SectionActivity from "./body/section-field-activity";
 import SectionCustomerCentric from "./body/section-customer-centric";
+import SectionBrandStory from "./body/section-brand-story";
 
 const AboutPage = () => {
   return (
     <main>
       <SectionHeader />
+      <SectionBrandStory />
       <SectionCustomerCentric />
       <SectionActivity />
       <SectionOurPartner />
