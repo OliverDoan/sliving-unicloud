626d75d29a0cf09254696d13bb6a0b8d28918af3
OliverDoan <doanvietthuan1005@gmail.com>
2022-05-05 10:34:28 +0700

    add section our partner

--- src/pages/about-us-v3/index.jsx
+++ src/pages/about-us-v3/index.jsx
@@ -5,11 +5,13 @@ import "./style.scss";
 import SectionHeader from "./body/section-header";
 import SectionIntro from "./body/section-intro";
 import SectionHelpMore from "../../components/help-more/section-help-more";
+import SectionOurPartner from "./body/section-our-partner";
 
 const AboutPage = () => {
   return (
     <main>
       <SectionHeader />
+      <SectionOurPartner />
       <SectionHelpMore />
     </main>
   );
