c7acb9fa53240fc7d01107eb469a8ab1d047ac5e
OliverDoan <doanvietthuan1005@gmail.com>
2022-05-05 09:18:53 +0700

    add section help more in aboutUsPage

--- src/pages/about-us-v3/index.jsx
+++ src/pages/about-us-v3/index.jsx
@@ -4,11 +4,13 @@ import "./style.scss";
 
 import SectionHeader from "./body/section-header";
 import SectionIntro from "./body/section-intro";
+import SectionHelpMore from "../../components/help-more/section-help-more";
 
 const AboutPage = () => {
   return (
     <main>
       <SectionHeader />
+      <SectionHelpMore />
     </main>
   );
 };
