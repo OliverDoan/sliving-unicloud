0ae500f7e98f2a8075a636fc0ab1680263c019ec
OliverDoan <doanvietthuan1005@gmail.com>
2022-05-09 09:06:09 +0700

    create branch smart-home-v3

---
+++ src/pages/smart-home-v3/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionHelpMore from "../../components/help-more/section-help-more";
+
+function SmartHomePage() {
+  return (
+    <>
+      <SectionHelpMore />
+    </>
+  );
+}
+
+export default SmartHomePage;
