58275797349182b64701cf5104cf38782f7a673c
quangcm <chauminhquang99@gmail.com>
2022-05-13 10:54:46 +0700

    create banner section for smart home v3

--- src/pages/smart-home-v3/body/section-products-slider.jsx
+++ src/pages/smart-home-v3/body/section-products-slider.jsx
@@ -4,38 +4,41 @@ import SwiperCore, { Navigation } from "swiper";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// import switchItem1 from "@images/about-us-v3/png/rectangle-switch.png";
-import switchItem1 from "../../../images/about-us-v3/png/rectangle-switch.png";
+// import switchItem1 from "@images/smart-home-v3/png/rectangle-switch.png";
+import switchItem1 from "../../../images/smart-home-v3/png/rectangle-switch.png";
 
-// import switchItem2 from "@images/about-us-v3/png/square-switch.png";
-import switchItem2 from "../../../images/about-us-v3/png/square-switch.png";
+// import switchItem2 from "@images/smart-home-v3/png/square-switch.png";
+import switchItem2 from "../../../images/smart-home-v3/png/square-switch.png";
 
-// import switchItem3 from "@images/about-us-v3/png/sensor-switch.png";
-import switchItem3 from "../../../images/about-us-v3/png/sensor-switch.png";
+// import switchItem3 from "@images/smart-home-v3/png/sensor-switch.png";
+import switchItem3 from "../../../images/smart-home-v3/png/sensor-switch.png";
 
-// import switchItem4 from "@images/about-us-v3/png/script-switch.png";
-import switchItem4 from "../../../images/about-us-v3/png/script-switch.png";
+// import switchItem4 from "@images/smart-home-v3/png/script-switch.png";
+import switchItem4 from "../../../images/smart-home-v3/png/script-switch.png";
 
-// import switchItem5 from "@images/about-us-v3/png/curtain-motor.png";
-import switchItem5 from "../../../images/about-us-v3/png/curtain-motor.png";
+// import switchItem5 from "@images/smart-home-v3/png/curtain-motor.png";
+import switchItem5 from "../../../images/smart-home-v3/png/curtain-motor.png";
 
-// import switchItem6 from "@images/about-us-v3/png/gate-way.png";
-import switchItem6 from "../../../images/about-us-v3/png/gate-way.png";
+// import switchItem6 from "@images/smart-home-v3/png/gate-way.png";
+import switchItem6 from "../../../images/smart-home-v3/png/gate-way.png";
 
-// import switchItem7 from "@images/about-us-v3/png/gateway-slug.png";
-import switchItem7 from "../../../images/about-us-v3/png/gateway-slug.png";
+// import switchItem7 from "@images/smart-home-v3/png/gateway-slug.png";
+import switchItem7 from "../../../images/smart-home-v3/png/gateway-slug.png";
 
-// import switchItemTelephone from "@images/about-us-v3/png/telephone-switch.png";
-import switchItemTelephone from "../../../images/about-us-v3/png/telephone-switch.png";
+// import switchItemTelephone from "@images/smart-home-v3/png/telephone-switch.png";
+import switchItemTelephone from "../../../images/smart-home-v3/png/telephone-switch.png";
 
-// import switchItemTelephoneBg from "@images/about-us-v3/svg/telephone-switch-bg.svg";
-import switchItemTelephoneBg from "../../../images/about-us-v3/svg/telephone-switch-bg.svg";
+// import appStoreItem from "@images/smart-home-v3/png/download-app-store.png";
+import appStoreItem from "../../../images/smart-home-v3/png/download-app-store.png";
 
-// import BigArrowLeft from "@images/about-us-v3/svg/big-arrow-left.svg";
-import BigArrowLeft from "../../../images/about-us-v3/svg/big-arrow-left.svg";
+// import ggPlayItem from "@images/smart-home-v3/png/download-gg-play.png";
+import ggPlayItem from "../../../images/smart-home-v3/png/download-gg-play.png";
 
-// import BigArrowRight from "@images/about-us-v3/svg/big-arrow-right.svg";
-import BigArrowRight from "../../../images/about-us-v3/svg/big-arrow-right.svg";
+// import BigArrowLeft from "@images/smart-home-v3/svg/big-arrow-left.svg";
+import BigArrowLeft from "../../../images/smart-home-v3/svg/big-arrow-left.svg";
+
+// import BigArrowRight from "@images/smart-home-v3/svg/big-arrow-right.svg";
+import BigArrowRight from "../../../images/smart-home-v3/svg/big-arrow-right.svg";
 
 SwiperCore.use([Navigation]);
 
@@ -48,52 +51,41 @@ const listSlide = [
       {
         id: 0,
         title: "Trải nghiệm mới từ lớp vỏ truyền thống:",
-        desc: "Với ngoại hình của dòng công tắc cơ truyền thống đi kèm với các tính năng hiện đại, sản phẩm tạo cảm giác gần gũi, quen thuộc nhưng mang lại trải nghiệm hoàn toàn mới mẻ và tiện lợi.",
+        desc: " Với ngoại hình của dòng công tắc cơ truyền thống đi kèm với các tính năng hiện đại, sản phẩm tạo cảm giác gần gũi, quen thuộc nhưng mang lại trải nghiệm hoàn toàn mới mẻ và tiện lợi.",
       },
       {
         id: 1,
         title: "Dễ sử dụng:",
-        desc: "Thông qua mạng Wifi/ Zigbee kết nối các thiết bị chỉ qua vài bước thiết lập đơn giản, giúp bạn bước vào một không gian Smart Living đẳng cấp.",
+        desc: " Thông qua mạng Wifi/ Zigbee kết nối các thiết bị chỉ qua vài bước thiết lập đơn giản, giúp bạn bước vào một không gian Smart Living đẳng cấp.",
       },
       {
         id: 2,
         title: "Tính năng vượt trội:",
-        desc: "Ngoài tính năng điều khiển bật/ tắt cục bộ như công tắc thường, sản phẩm còn có khả năng kết nối thông qua Gateway để điều khiển trên Internet. Chạy kịch bản bằng thao tác nhấn đơn, nhấn đúp, nhấn giữ, thay đổi các thông số độ nhạy, độ sáng, thông báo trạng thái của thiết bị trên Gateway.",
+        desc: " Ngoài tính năng điều khiển bật/ tắt cục bộ như công tắc thường, sản phẩm còn có khả năng kết nối thông qua Gateway để điều khiển trên Internet. Chạy kịch bản bằng thao tác nhấn đơn, nhấn đúp, nhấn giữ, thay đổi các thông số độ nhạy, độ sáng, thông báo trạng thái của thiết bị trên Gateway.",
       },
     ],
     listItem: [
       {
         id: 0,
-        titles: [
-          {
-            id: 0,
-            title: "Công tắc hình chữ nhật",
-          },
-          {
-            id: 1,
-
-            title: "Công tắc hình vuông",
-          },
-        ],
         img: switchItem1,
       },
       {
         id: 1,
-        titles: [
-          {
-            id: 0,
-            title: "Công tắc hình chữ nhật",
-          },
-          {
-            id: 1,
-
-            title: "Công tắc hình vuông",
-          },
-        ],
         img: switchItem2,
       },
     ],
-    imgObject: null,
+    titles: [
+      {
+        id: 0,
+        title: "Công tắc hình chữ nhật",
+      },
+      {
+        id: 1,
+        title: "Công tắc hình vuông",
+      },
+    ],
+    phoneImg: null,
+    downloadObjects: [],
   },
   {
     id: 1,
@@ -103,46 +95,37 @@ const listSlide = [
       {
         id: 0,
         title: "Thiết kế sang trọng:",
-        desc: "Sản phẩm có mặt kính cường lực chống trầy, thiết kế đơn giản cùng viền nhôm mạ vàng 18k cao cấp, các góc bo tròn an toàn. Công tắc được thiết kế tương thích với loại đế âm tường tạo cho người dùng thuận tiện lắp đặt và sử dụng.",
+        desc: " Sản phẩm có mặt kính cường lực chống trầy, thiết kế đơn giản cùng viền nhôm mạ vàng 18k cao cấp, các góc bo tròn an toàn. Công tắc được thiết kế tương thích với loại đế âm tường tạo cho người dùng thuận tiện lắp đặt và sử dụng.",
       },
       {
         id: 1,
         title: "Tính năng vượt trội:",
-        desc: "Với tính năng bật/tắt cục bộ bằng cảm ứng điện dung, sản phẩm còn có khả năng chạy kịch bản bằng thao tác nhấn đơn, nhấn đúp, nhấn giữ, thay đổi các thông số độ nhạy độ sáng, thông báo trạng thái online lên Gateway.",
+        desc: " Với tính năng bật/tắt cục bộ bằng cảm ứng điện dung, sản phẩm còn có khả năng chạy kịch bản bằng thao tác nhấn đơn, nhấn đúp, nhấn giữ, thay đổi các thông số độ nhạy độ sáng, thông báo trạng thái online lên Gateway.",
       },
     ],
+
     listItem: [
       {
         id: 0,
-        titles: [
-          {
-            id: 0,
-            title: "Công tắc cảm ứng",
-          },
-          {
-            id: 1,
-            title: "Công tắc kịch bản",
-          },
-        ],
         img: switchItem3,
       },
       {
         id: 1,
-        titles: [
-          {
-            id: 0,
-            title: "Công tắc cảm ứng",
-          },
-          {
-            id: 1,
-            title: "Công tắc kịch bản",
-          },
-        ],
         img: switchItem4,
       },
     ],
-
-    imgObject: null,
+    titles: [
+      {
+        id: 0,
+        title: "Công tắc cảm ứng",
+      },
+      {
+        id: 1,
+        title: "Công tắc kịch bản",
+      },
+    ],
+    phoneImg: null,
+    downloadObjects: [],
   },
   {
     id: 2,
@@ -152,22 +135,23 @@ const listSlide = [
       {
         id: 0,
         title: "Thiết kế dễ dàng lắp đặt và sử dụng:",
-        desc: "Toàn bộ sản phẩm bên ngoài được làm bằng nhôm cao cấp, khả năng chịu lực va chạm mạnh, chịu nhiệt và chống ăn mòn cùng với kích thước nhỏ gọn nên phù hợp với nhiều loại hình không gian khác nhau.",
+        desc: " Toàn bộ sản phẩm bên ngoài được làm bằng nhôm cao cấp, khả năng chịu lực va chạm mạnh, chịu nhiệt và chống ăn mòn cùng với kích thước nhỏ gọn nên phù hợp với nhiều loại hình không gian khác nhau.",
       },
       {
         id: 1,
         title: "Hoạt động dựa trên tương tác vật lý:",
-        desc: "Với động cơ rèm thông minh, người dùng dễ dàng điều khiển từ xa thông qua ứng dụng hoặc chạy kịch bản mở/đóng theo nhu cầu sử dụng. Khi gặp sự cố về điện, thiết bị vẫn có thể hoạt động bình thường thông qua mở và đóng vật lý dễ dàng.",
+        desc: " Với động cơ rèm thông minh, người dùng dễ dàng điều khiển từ xa thông qua ứng dụng hoặc chạy kịch bản mở/đóng theo nhu cầu sử dụng. Khi gặp sự cố về điện, thiết bị vẫn có thể hoạt động bình thường thông qua mở và đóng vật lý dễ dàng.",
       },
     ],
     listItem: [
       {
         id: 0,
-        titles: [],
         img: switchItem5,
       },
     ],
-    imgObject: null,
+    titles: [],
+    phoneImg: null,
+    downloadObjects: [],
   },
   {
     id: 3,
@@ -177,45 +161,36 @@ const listSlide = [
       {
         id: 0,
         title: "Kết nối ổn định & tương thích đa dạng:",
-        desc: "Ngoài tính năng điều khiển bật/ tắt cục bộ như công tắc thường, sản phẩm còn có khả năng kết nối thông qua Gateway để điều khiển trên Internet. Chạy kịch bản bằng thao tác nhấn đơn, nhấn đúp, nhấn giữ, thay đổi các thông số độ nhạy, độ sáng, thông báo trạng thái của thiết bị trên Gateway.",
+        desc: " Ngoài tính năng điều khiển bật/ tắt cục bộ như công tắc thường, sản phẩm còn có khả năng kết nối thông qua Gateway để điều khiển trên Internet. Chạy kịch bản bằng thao tác nhấn đơn, nhấn đúp, nhấn giữ, thay đổi các thông số độ nhạy, độ sáng, thông báo trạng thái của thiết bị trên Gateway.",
       },
       {
         id: 1,
         title: "Độ bảo mật cao:",
-        desc: "Sử dụng công nghệ mã hoá và xác thực đầu cuối, khi điều khiển các thiết bị từ xa thì dữ liệu có liên quan sẽ được mã hoá trong quá trình gửi. Vì vậy đảm bảo quyền riêng tư và tính bảo mật cho dữ liệu người dùng.",
+        desc: " Sử dụng công nghệ mã hoá và xác thực đầu cuối, khi điều khiển các thiết bị từ xa thì dữ liệu có liên quan sẽ được mã hoá trong quá trình gửi. Vì vậy đảm bảo quyền riêng tư và tính bảo mật cho dữ liệu người dùng.",
       },
     ],
     listItem: [
       {
         id: 0,
-        titles: [
-          {
-            id: 0,
-            title: "Gateway",
-          },
-          {
-            id: 1,
-            title: "Gateway Slug",
-          },
-        ],
         img: switchItem6,
       },
       {
         id: 1,
-        titles: [
-          {
-            id: 0,
-            title: "Gateway",
-          },
-          {
-            id: 1,
-            title: "Gateway Slug",
-          },
-        ],
         img: switchItem7,
       },
     ],
-    imgObject: null,
+    titles: [
+      {
+        id: 0,
+        title: "Gateway",
+      },
+      {
+        id: 1,
+        title: "Gateway Slug",
+      },
+    ],
+    phoneImg: null,
+    downloadObjects: [],
   },
   {
     id: 4,
@@ -225,25 +200,86 @@ const listSlide = [
       {
         id: 0,
         title: "Tính năng vượt trội:",
-        desc: "Thiết kế nhỏ gọn, tinh tế giúp người tiêu dùng dễ dàng sử dụng, dễ sửa chữa và bảo hành. Thiết bị có thể thay thế remote điều khiển của TV, quạt, điều hoà, rèm cửa,... bằng cách đọc lệnh IR, RF. Hỗ trợ điều khiển từ xa thông qua ứng dụng Sliving, giọng nói, chạy kịch bản hoặc qua trợ lý ảo như Google Assistant, Siri, Alexa,...",
+        desc: " Thiết kế nhỏ gọn, tinh tế giúp người tiêu dùng dễ dàng sử dụng, dễ sửa chữa và bảo hành. Thiết bị có thể thay thế remote điều khiển của TV, quạt, điều hoà, rèm cửa,... bằng cách đọc lệnh IR, RF. Hỗ trợ điều khiển từ xa thông qua ứng dụng Sliving, giọng nói, chạy kịch bản hoặc qua trợ lý ảo như Google Assistant, Siri, Alexa,...",
       },
       {
         id: 1,
         title: "Giám sát & cảnh báo:",
-        desc: "Kiểm soát năng lượng tiêu dùng thông minh nhờ có tính năng kết nối hai chiều với dàn lạnh, vừa có khả năng điều khiển và giám sát các thông số môi trường như nhiệt độ, độ ẩm,... và đưa ra các cảnh báo kịp thời đến người dùng.",
+        desc: " Kiểm soát năng lượng tiêu dùng thông minh nhờ có tính năng kết nối hai chiều với dàn lạnh, vừa có khả năng điều khiển và giám sát các thông số môi trường như nhiệt độ, độ ẩm,... và đưa ra các cảnh báo kịp thời đến người dùng.",
       },
     ],
     listItem: [],
-    imgObject: {
-      mainImg: switchItemTelephone,
-      backgroundImg: switchItemTelephoneBg,
+    titles: [],
+    phoneImg: {
+      img: switchItemTelephone,
     },
+    downloadObjects: [
+      {
+        id: 0,
+        img: appStoreItem,
+      },
+      {
+        id: 1,
+        img: ggPlayItem,
+      },
+    ],
   },
 ];
 
-const SectionProductsSlider = () => {
+const ProductItemSwiper = ({ slide }) => {
   const [swiper, setSwiper] = useState(null);
+  const [swiperIndex, setSwiperIndex] = useState(0);
+  return (
+    <div className="product-left-content">
+      {slide?.listItem && slide?.listItem.length > 0 && (
+        <Swiper
+          navigation={{
+            prevEl: ".prev",
+            nextEl: ".next",
+          }}
+          className="banner-products-slider"
+          slidesPerView={1}
+          onSwiper={(s) => {
+            setSwiper(s);
+          }}
+          onSlideChange={(s) => {
+            setSwiperIndex(s.realIndex);
+          }}
+        >
+          {slide?.listItem.map((slideItem) => (
+            <>
+              <SwiperSlide key={slideItem.id}>
+                <img src={slideItem.img} alt="" />
+              </SwiperSlide>
+            </>
+          ))}
+        </Swiper>
+      )}
+      {slide?.titles && slide?.titles.length > 0 && (
+        <div className="banner-products-btn-group">
+          {slide?.titles.map((btnItem) => {
+            return (
+              <div
+                className={`${
+                  swiperIndex === btnItem.id
+                    ? "products-btn-item active"
+                    : "products-btn-item"
+                } `}
+                onClick={() => {
+                  swiper.slideTo(btnItem.id);
+                }}
+              >
+                <span className="product-item-title">{btnItem.title}</span>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
 
+const SectionProductsSlider = () => {
   const [toggleState, setToggleState] = useState(0);
 
   const increaseToggleTab = () => {
@@ -285,66 +321,48 @@ const SectionProductsSlider = () => {
             }
             key={slide.id}
           >
-            <div className="left-content-container">
-              <div className="product-left-content">
-                <Swiper
-                  navigation={{
-                    prevEl: ".prev",
-                    nextEl: ".next",
-                  }}
-                  className="banner-products-slider"
-                  slidesPerView={1}
-                  onSwiper={(s) => {
-                    setSwiper(s);
-                  }}
-                >
-                  {slide?.listItem &&
-                    slide?.listItem.map((slideItem) => (
-                      <SwiperSlide key={slideItem.id}>
-                        <div className="banner-products-container">
+            <div className="left-content-container animate fadeUp">
+              {!slide.phoneImg && slide?.downloadObjects?.length === 0 && (
+                <ProductItemSwiper slide={slide} />
+              )}
+              {slide?.phoneImg && (
+                <div className="product-phone-wrapper">
+                  <img src={slide.phoneImg.img} alt=""></img>
+                  {slide?.downloadObjects?.length > 0 && (
+                    <div className="download-apps-container">
+                      {slide?.downloadObjects.map((object) => (
+                        <div className="download-app-item">
                           <img
-                            src={slideItem.img}
+                            src={object.img}
                             alt=""
-                            onClick={() => {
-                              swiper.slideNext();
-                              console.log(swiper);
-                            }}
-                          />
-                          <div className="banner-products-btn-group">
-                            {slideItem?.titles &&
-                              slideItem?.titles.map((btnItem) => {
-                                return (
-                                  <div className="products-btn-item">
-                                    {btnItem.title}
-                                  </div>
-                                );
-                              })}
-                          </div>
+                            className="download-app-img"
+                          ></img>
                         </div>
-                      </SwiperSlide>
-                    ))}
-                </Swiper>
-              </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              )}
             </div>
 
-            <div className="right-content-container">
+            <div className="right-content-container animate fadeUp">
               <div className="product-right-content">
-                <h4>{slide.subTitle}</h4>
-                <h1>{slide.title}</h1>
-                {slide?.contents &&
-                  slide?.contents.map((content) => (
-                    <p>
-                      <p style={{ display: "inline-block" }}>{content.title}</p>
-                      {content.desc}
-                    </p>
-                  ))}
+                <h4 className="product-right-subtitle">{slide.subTitle}</h4>
+                <h1 className="product-right-title">{slide.title}</h1>
+                <div className="product-right-divider"></div>
+                <div className="product-right-all-contents">
+                  {slide?.contents &&
+                    slide?.contents.map((content) => (
+                      <p className="product-right-desc-container">
+                        <span className="product-right-main-title">
+                          {content.title}
+                        </span>
+                        {content.desc}
+                      </p>
+                    ))}
+                </div>
                 <div className="btn-group">
-                  <button
-                    className="advise-now-btn"
-                    onClick={() => {
-                      swiper.slideNext();
-                    }}
-                  >
+                  <button className="advise-now-btn">
                     <span>Tư vấn ngay</span>
                   </button>
                   <button type="button" className="learn-more-btn">
