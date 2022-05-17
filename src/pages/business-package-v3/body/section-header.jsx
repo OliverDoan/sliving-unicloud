a487dfa89df795aee24f8b638a2783c87845079a
OliverDoan <doanvietthuan1005@gmail.com>
2022-05-17 11:34:21 +0700

    add section header in business package page

---
+++ src/pages/business-package-v3/body/section-header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import bgCloud from "../../../images/business-package-v3/bg-cloud.png";
+import bgLinear from "../../../images/business-package-v3/bg-linear.png";
+import bgBuilding from "../../../images/business-package-v3/building.png";
+function SectionHeader() {
+  return (
+    <header className="section-business-package-v3 ">
+      <img src={bgCloud} alt="" />
+      <img src={bgLinear} alt="" />
+      <img src={bgBuilding} alt="" />
+      <div className="container-v3">
+        <Row>
+          <Col md={5}>
+            <h2 className="text-white business-package-title">Gói Giải Pháp</h2>
+            <h4 className="text-white business-package-subtitle">
+              Dành Cho Doanh Nghiệp
+            </h4>
+            <p className="text-white business-package-des">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              vulputate libero et velit interdum, ac aliquet odio mattis. Class
+              aptent taciti sociosqu ad litora torquent per conubia nostra, per
+              inceptos himenaeos.
+            </p>
+          </Col>
+          <Col md={7}></Col>
+        </Row>
+      </div>
+    </header>
+  );
+}
+
+export default SectionHeader;
---
+++ src/pages/business-package-v3/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionHeader from "./body/section-header";
+import "./style.scss";
+function BusinessPackagePage() {
+  return (
+    <>
+      <SectionHeader />
+    </>
+  );
+}
+
+export default BusinessPackagePage;
